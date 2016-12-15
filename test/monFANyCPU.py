57298bb6dd4f65132aabd4bf6945ac21fcdef4cf
TRuHa <esetruha@gmail.com>
2016-12-15 23:13:41 +0100

    Simplificando codigo

--- test/monFANyCPU.py
+++ test/monFANyCPU.py
@@ -5,6 +5,7 @@ import commands
 import time
 import os
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 pwm = GPIO.PWM(17, 1000)
@@ -19,40 +20,25 @@ pwm.start(50)
 os.system('clear')
 
 while True:
-	temperatura = round(get_cpu_temp())
-	if temperatura <= 38:
+	temp = round(get_cpu_temp())
+	time.sleep(1)
+	os.system('clear')
+	print "Temperatura CPU: ", temp
+	if temp <= 38:
 		pwm.ChangeDutyCycle(50)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 50%"
-		time.sleep(1)
-	elif temperatura >= 38 and temperatura <= 40:    
+	elif temp >= 38 and temp <= 40:    
 		pwm.ChangeDutyCycle(40)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 60%"
-		time.sleep(1)
-	elif temperatura >= 41 and temperatura <= 43:  
+	elif temp >= 41 and temp <= 43:  
 		pwm.ChangeDutyCycle(30)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 70%"
-		time.sleep(1)
-	elif temperatura >= 44 and temperatura <= 46:
+	elif temp >= 44 and temp <= 46:
 		pwm.ChangeDutyCycle(20)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 80%"
-		time.sleep(1)
-	elif temperatura >= 47 and temperatura <= 49:
+	elif temp >= 47 and temp <= 49:
 		pwm.ChangeDutyCycle(10)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 90%"
-		time.sleep(1)
-	elif temperatura >= 49:
+	elif temp >= 49:
 		pwm.ChangeDutyCycle(0)
-		os.system('clear')
-		print "Temperatura CPU: ", temperatura
 		print "Ventilador al 100%"
-		time.sleep(1)
