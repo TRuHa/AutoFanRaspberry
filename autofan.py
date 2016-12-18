f8b76d3e2ad6c0421133e9c1ee98038a7826d0c2
TRuHa <esetruha@gmail.com>
2016-12-18 14:57:20 +0100

    compatibilidad Kodi

--- autofan.py
+++ autofan.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 #Version AutoFan1.0
 
+distro = open("/etc/issue","r")
+if distro.read() < "OpenELEC":
+	import sys
+	sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib')
+distro.close()
+
 import RPi.GPIO as GPIO
 import commands
 import time
@@ -17,18 +23,24 @@ def get_cpu_temp():
 
 pwm.start(50)
 
-while True:
-	temp = round(get_cpu_temp())
-	if temp <= 38:
-		pwm.ChangeDutyCycle(50)
-	elif temp >= 38 and temp <= 40:
-		pwm.ChangeDutyCycle(40)
-	elif temp >= 41 and temp <= 43:
-		pwm.ChangeDutyCycle(30)
-	elif temp >= 44 and temp <= 46:
-		pwm.ChangeDutyCycle(20)
-	elif temp >= 47 and temp <= 49:
-		pwm.ChangeDutyCycle(10)
-	elif temp >= 49:
-		pwm.ChangeDutyCycle(0)
-	time.sleep(30)
+try:
+	while True:
+		temp = round(get_cpu_temp())
+		if temp <= 38:
+			pwm.ChangeDutyCycle(50)
+		elif temp >= 38 and temp <= 40:
+			pwm.ChangeDutyCycle(40)
+		elif temp >= 41 and temp <= 43:
+			pwm.ChangeDutyCycle(30)
+		elif temp >= 44 and temp <= 46:
+			pwm.ChangeDutyCycle(20)
+		elif temp >= 47 and temp <= 49:
+			pwm.ChangeDutyCycle(10)
+		elif temp >= 49:
+			pwm.ChangeDutyCycle(0)
+		time.sleep(30)
+
+except KeyboardInterrupt:
+	print ""
+	pwm.stop()
+	GPIO.cleanup()
