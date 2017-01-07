98db04b1ccca862bb58966c3c793a4b1cb065e83
TRuHa <esetruha@gmail.com>
2017-01-07 13:52:15 +0100

    Codigo terminado

--- autofan.py
+++ autofan.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-#Version AutoFan1.0
 
-import RPi.GPIO as GPIO
+import sys
 import commands
 import time
+#sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib') # Si el sistema es Kodi habilitar esta linea
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
@@ -17,18 +18,24 @@ def get_cpu_temp():
 
 pwm.start(50)
 
-while True:
-	temp = round(get_cpu_temp())
-	if temp <= 38:
-		pwm.ChangeDutyCycle(50)
-	elif temp >= 38 and temp <= 40:
-		pwm.ChangeDutyCycle(40)
-	elif temp >= 41 and temp <= 43:
-		pwm.ChangeDutyCycle(30)
-	elif temp >= 44 and temp <= 46:
-		pwm.ChangeDutyCycle(20)
-	elif temp >= 47 and temp <= 49:
-		pwm.ChangeDutyCycle(10)
-	elif temp >= 49:
-		pwm.ChangeDutyCycle(0)
-	time.sleep(30)
+try:
+	while True:
+		temp = round(get_cpu_temp())
+		if temp <= 38:
+			pwm.ChangeDutyCycle(50)
+		elif temp >= 38 and temp <= 40:
+			pwm.ChangeDutyCycle(40)
+		elif temp >= 41 and temp <= 43:
+			pwm.ChangeDutyCycle(30)
+		elif temp >= 44 and temp <= 46:
+			pwm.ChangeDutyCycle(20)
+		elif temp >= 47 and temp <= 49:
+			pwm.ChangeDutyCycle(10)
+		elif temp >= 49:
+			pwm.ChangeDutyCycle(0)
+		time.sleep(30)
+
+except KeyboardInterrupt:
+	print ""
+	pwm.stop()
+	GPIO.cleanup()
--- testautofan.py
+++ testautofan.py
@@ -4,17 +4,27 @@ import commands
 import time
 import sys
 import os
+#sys.path.append('/storage/.kodi/addons/python.RPi.GPIO/lib') # Si el sistema es Kodi habilitar esta linea
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+pwm = GPIO.PWM(17, 1000)
+fan = 50
+
 
 def get_cpu_temp():
-	tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
-	cpu_temp = tempFile.read()
-	tempFile.close()
-	return float(cpu_temp)/1000
+    tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
+    cpu_temp = tempFile.read()
+    tempFile.close()
+    return float(cpu_temp)/1000
 
+pwm.start(50)
 os.system('clear')
 
 try:
 	while True:
+		os.system('clear')
 		temp = round(get_cpu_temp())
 		print ""
 		print "-------------------------------------------------------------"
@@ -30,17 +40,35 @@ try:
 		print ""
 		print "  Este script hace un test de AutoFan.                       "
 		print ""
-		print "  Con el puede realizar un monitoreo de la temperatura       "
-		print "  y comprobar la eficacia del mismo.                         "
+		print "  Con el puede comprobar el correcto funcionamiento,         "
+		print "  monitoreo de temperaturas y rendimiento del ventilador.    "
 		print ""
 		print "-------------------------------------------------------------"
-		print "                  Temperatura CPU: ", temp
+		print "      Temperatura CPU: ", temp, " |  Ventilador al", fan,"%"
 		print "-------------------------------------------------------------"
 		print ""
 		print "Presione [CTRL+C] para abortar."
 		time.sleep(1)
-		os.system('clear')
+		if temp <= 38:
+			pwm.ChangeDutyCycle(50)
+			fan = 50
+		elif temp >= 38 and temp <= 40:    
+			pwm.ChangeDutyCycle(40)
+			fan = 60
+		elif temp >= 41 and temp <= 43:  
+			pwm.ChangeDutyCycle(30)
+			fan = 70
+		elif temp >= 44 and temp <= 46:
+			pwm.ChangeDutyCycle(20)
+			fan = 80
+		elif temp >= 47 and temp <= 49:
+			pwm.ChangeDutyCycle(10)
+			fan = 90
+		elif temp >= 49:
+			pwm.ChangeDutyCycle(0)
+			fan = 100
 
 except KeyboardInterrupt:
 	print ""
-
+	pwm.stop()
+	GPIO.cleanup()
